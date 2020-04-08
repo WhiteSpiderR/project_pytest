546b19d900306e2827c8ec4c85346cd09ee6ed86
Kirill.Nosacheev <knosachev@script-master.net>
2020-04-08 21:17:43 +0300

    new project

--- sdex_ui_test_pytest/CommonConstants/Common.py
+++ sdex_ui_test_pytest/CommonConstants/Common.py
@@ -2,11 +2,11 @@ class CommonConstants:
     """
     Класс с общими константами
     """
-    LOGIN_SDEX_2 = "sdexauto+2@gmail.com"  # Логин для авторизации (2-й уровень верификации)
-    PASSWORD_SDEX_2 = "waeYiofoh7oom2ah!"  # Пароль для авторизации (2-й уровень верификации) к логину sdexauto+2@gmail.com
-    DEFAULT_EMAIL_CODE = '123456'  # Пароля для воода при двухфакторной аутентификации
+    LOGIN_SDEX_2 = ""  # Логин для авторизации (2-й уровень верификации)
+    PASSWORD_SDEX_2 = ""  # Пароль для авторизации (2-й уровень верификации) к логину sdexauto+2@gmail.com
+    DEFAULT_EMAIL_CODE = ''  # Пароля для воода при двухфакторной аутентификации
 
-    EXPECTED_TEXT_OF_PAGE_AFTER_LOGIN_ACCOUNT_2 = 'Markets'
+    EXPECTED_TEXT_OF_PAGE_AFTER_LOGIN_ACCOUNT_2 = ''
 
-    SITE_HOST = "https://webfront-develop.kube.n-t.io"
+    SITE_HOST = ""
     SITE_HOST_DM = f"{SITE_HOST}/DM"
